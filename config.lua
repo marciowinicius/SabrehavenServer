98671b23b32b888f48b2bc4e53fb0a54b178bbd5
Márcio <marciowinicius.mw@gmail.com>
2023-01-23 23:45:08 -0300

    First.

--- config.lua
+++ config.lua
@@ -6,10 +6,10 @@ paladinRangeAttackDamageIncreasePercent = -1
 minRateSpawn = 100
 maxRateSpawn = 200
 corpseOwnerEnabled = false
-uhTrap = false
-ropeSpotBlock = false
+uhTrap = true
+ropeSpotBlock = true
 showMonsterLoot = true
-blockHeight = false
+blockHeight = true
 dropItems = false
 marketOfferDuration = 7 * 24 * 60 * 60
 premiumToCreateMarketOffer = false
@@ -17,25 +17,25 @@ premiumToCreateMarketOffer = false
 -- Combat settings
 -- NOTE: valid values for worldType are: "pvp", "no-pvp" and "pvp-enforced"
 worldType = "pvp"
-hotkeyAimbotEnabled = true
-protectionLevel = 1
+hotkeyAimbotEnabled = false
+protectionLevel = 15
 pzLocked = 60000
 removeChargesFromRunes = true
 stairJumpExhaustion = 0
-experienceByKillingPlayers = true
+experienceByKillingPlayers = false
 expFromPlayersLevelRange = 50
 distanceWeaponsDropOnGround = false
 
 -- Skull System
-banLength = 2 * 24 * 60 * 60
+banLength = 3 * 24 * 60 * 60
 whiteSkullTime = 15 * 60
-redSkullTime = 2 * 24 * 60 * 60
-killsDayRedSkull = 4
-killsWeekRedSkull = 12
-killsMonthRedSkull = 35
-killsDayBanishment = 7
-killsWeekBanishment = 18
-killsMonthBanishment = 60
+redSkullTime = 3 * 24 * 60 * 60
+killsDayRedSkull = 8
+killsWeekRedSkull = 24
+killsMonthRedSkull = 70
+killsDayBanishment = 10
+killsWeekBanishment = 30
+killsMonthBanishment = 90
 
 -- Connection Config
 -- NOTE: maxPlayers set to 0 means no limit
@@ -45,10 +45,10 @@ loginProtocolPort = 7171
 gameProtocolPort = 7172
 statusProtocolPort = 7171
 maxPlayers = 0
-motd = "Welcome to Tibianus!"
+motd = "Welcome to DragonsAge!"
 onePlayerOnlinePerAccount = true
 allowClones = false
-serverName = "Tibianus"
+serverName = "DragonsAge"
 statusTimeout = 5000
 replaceKickOnLogin = true
 maxPacketsPerSecond = 50
@@ -75,12 +75,13 @@ mapName = "map"
 mapAuthor = "CipSoft"
 
 -- MySQL
-mysqlHost = "127.0.0.1"
+mysqlHost = "localhost"
 mysqlUser = "root"
-mysqlPass = ""
-mysqlDatabase = "sabrehaven"
+mysqlPass = "Winicius-1"
+mysqlDatabase = "marcioold"
 mysqlPort = 3306
-mysqlSock = ""
+mysqlSock = "/var/run/mysqld/mysqld.sock"
+passwordType = "sha1"
 
 -- Misc.
 allowChangeOutfit = true
@@ -96,11 +97,11 @@ newbieLevelThreshold = 5
 
 -- Rates
 -- NOTE: rateExp is not used if you have enabled stages in data/XML/stages.xml
-rateExp = 1
-rateSkill = 3
-rateLoot = 2
-rateMagic = 2
-rateSpawn = 0
+rateExp = 200
+rateSkill = 50
+rateLoot = 10
+rateMagic = 35
+rateSpawn = 2
 
 -- Monsters
 deSpawnRange = 2
@@ -117,7 +118,7 @@ defaultPriority = "high"
 startupDatabaseOptimization = true
 
 -- Status server information
-ownerName = "Erikas"
+ownerName = "Dragonas"
 ownerEmail = ""
-url = "https://tibianus.com/"
-location = "Poland"
+url = "https://dragonsage.com/"
+location = "Brazil"
